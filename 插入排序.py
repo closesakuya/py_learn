b3e20cd4f7e8af329928996cc25f4a5d6b243f88
closesakuya <closesakuya@sina.com>
2019-10-14 16:14:52 +0800

    edit charu

--- 插入排序.py
+++ 插入排序.py
@@ -2,15 +2,19 @@
 def insert_sort( lst ):
     for i in range(1,len(lst)):
         insert_val = lst[i]
+
         for j in range(i)[::-1]:
             if lst[j] > insert_val:
                 lst[j+1] = lst[j]
             else:
-                lst[j+1] = insert_val
                 break
+        else:   #!!!!循环正常执行完，未break则进入
+            j -= 1
+            
+        lst[j+1] = insert_val
                 
 
-a = [1,3,5,1,8,3,5,1,9,121]
+a = [777,555,1,3,5,1,8,3,5,1,9,121]
 insert_sort(a)
 print(a)
               
